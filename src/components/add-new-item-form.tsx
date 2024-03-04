faf95f01cc5c46ce4c10cf22596e15e0556ad340
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2024-03-04 01:24:07 -0300

    styles: add responsiveness

--- src/components/add-new-item-form.tsx
+++ src/components/add-new-item-form.tsx
@@ -5,8 +5,8 @@ import { ItemInput } from './ui/item-input'
 
 export function AddNewItemForm() {
   return (
-    <form className="grid grid-cols-[1fr_150px_160px_40px] items-end gap-3 text-gray-100 leading-none text-sm">
-      <ItemInput />
+    <form className="grid grid-cols-[1fr_160px_40px] grid-rows-2 sm:grid-rows-1 sm:grid-cols-[1fr_150px_160px_40px] items-end gap-3 text-gray-100 leading-none text-sm">
+      <ItemInput className="col-span-3 sm:col-span-1" />
 
       <QuantityInput />
 
